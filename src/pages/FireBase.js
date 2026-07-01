e02aa31cf260ee0dcfe7d448538fd80ad747aa66
budhathoki10 <budhathokikushal170@gmail.com>
2026-07-01 11:28:27 +0545

    changed firebase l

--- src/pages/FireBase.js
+++ src/pages/FireBase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app";
 import { getAuth, OAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -13,16 +8,16 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
 const provider = new OAuthProvider("microsoft.com");
+
 provider.setCustomParameters({
-  
-  tenant: "e620b2cb-0129-44bb-9edd-aaa6e9057514",
+  tenant: "common",
   prompt: "select_account",
 });
 
